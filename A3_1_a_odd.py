bc0fcbb6861ee6a514e093dd0dc1c341f44e5a57
SaiSushmita <71960577+SaiSushmita@users.noreply.github.com>
2020-10-13 22:38:00 +0530

    Add files via upload

---
+++ A3_1_a_odd.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 20:07:30 2020
+
+@author: Sushmita
+"""
+
+only_odd=list(range(1,100))
+  
+only_odd = [num for num in only_odd if num % 2 == 1] 
+  
+print(only_odd) 
